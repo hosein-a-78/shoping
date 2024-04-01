83319679be407cb80e38790ec991669bbfc69afe
abed-a-78 <abed.re.k00021@gmail.com>
2024-04-01 08:07:30 -0700

    Creating a loader with react-spinner, creating a search box, creating a category filter, and also a product filter

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,85 @@
+import { useEffect, useState } from "react";
+
+//Icons
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
+
+//Components
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductsContext";
+import { filterProducts, searchProducts } from "../helpers/helper";
 
+//Styles
 import styles from "./ProductsPage.module.css"
 
 const ProductsPage = () => {
     const products = useProducts();
+
+    const [displayed, setDisplayed] = useState([]);
+
+    const [search, setSearch] = useState("");
+
+    const [query, setQuery] = useState({});
+
+    useEffect(() => {
+        setDisplayed(products)
+    }, [products]);
+
+    useEffect(() => {
+        let finalProducts = searchProducts(products, query.search);
+        finalProducts = filterProducts(finalProducts, query.category);
+        setDisplayed(finalProducts);
+    }, [query]);
+
+    const clickHandler = () => {
+        setQuery((query) => ({ ...query, search }));
+    };
+
+    const categoriHandler = (event) => {
+        const { tagName } = event.target;
+        const category = event.target.innerText.toLowerCase();
+        if (tagName !== "LI") return;
+        setQuery((query) => ({ ...query, category }));
+    };
+
+
     return (
-        <div className={styles.container}>
-            {!products.length && <Loader />}
-            <div className={styles.products}>
-                {products.map((p) => (
-                    <Card key={p.id} data={p} />
-                ))}
+        <>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={search}
+                    onChange={e => setSearch(e.target.value.toLowerCase().trim())}
+                />
+                <button onClick={clickHandler}>
+                    <ImSearch />
+                </button>
             </div>
-            <div>Sidbar</div>
-        </div>
+
+            <div className={styles.container}>
+                {!displayed.length && <Loader />}
+                <div className={styles.products}>
+                    {displayed.map((p) => (
+                        <Card key={p.id} data={p} />
+                    ))}
+                </div>
+                <div>
+                    <div>
+                        <FaListUl />
+                        <p>Categories</p>
+                    </div>
+                    <ul onClick={categoriHandler}>
+                        <li>All</li>
+                        <li>Electronics</li>
+                        <li>Jewelery</li>
+                        <li>Men's Clothing</li>
+                        <li>Women's Clothing</li>
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 };
 
